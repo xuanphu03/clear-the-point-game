270369c381428cc8fc4eb015373447262a2c5163
Xuan Phu <xuanphu1024@gmail.com>
2024-09-27 10:07:47 +0700

    feat(master): init project

---
+++ apps/src/pages/App.tsx
@@ -0,0 +1,11 @@
+import Heading from './components/heading';
+
+function App() {
+  return (
+    <div className='max-w-5xl mx-auto my-5'>
+      <Heading />
+    </div>
+  );
+}
+
+export default App;
---
+++ apps/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
+  ],
+  prefix: '',
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
